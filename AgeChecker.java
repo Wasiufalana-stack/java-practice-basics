e13e9ba97e515fcb640e860b4fdada487f16248f
Wasiufalana-stack <yisafataifalana@gmail.com>
2026-02-05 17:20:38 +0000

    Create AgeChecker.java

---
+++ AgeChecker.java
@@ -0,0 +1,11 @@
+public class Agechecker {
+  public static void main(String[] args) {
+      int age = 18;
+      if (age >= 18) {
+        Systen.out.println("You are an adult");
+      } else {
+        System.out.println("You are under 18");
+      }
+  }
+}
+    
